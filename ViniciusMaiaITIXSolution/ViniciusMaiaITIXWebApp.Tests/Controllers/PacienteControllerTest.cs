1808788b49f1af66830ff447682a1113fbf9222f
viniciuscmaia <vinicius.c.maia@gmail.com>
2018-12-09 23:19:32 -0200

    Criação de teste para PacienteController, remoção de arquivos inutilizados e finalização do projeto.

---
+++ ViniciusMaiaITIXSolution/ViniciusMaiaITIXWebApp.Tests/Controllers/PacienteControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViniciusMaiaITIXWebApp.Service;
+using ViniciusMaiaITIXWebApp.Controllers;
+using Moq;
+using ViniciusMaiaITIXWebApp.Models;
+using Newtonsoft.Json.Linq;
+
+namespace ViniciusMaiaITIXWebApp.Tests.Controllers
+{
+    [TestClass]
+    public class PacienteControllerTest
+    {
+        [TestClass]
+        public class ConsultaControllerTest
+        {
+            [TestMethod]
+            public void CamposObrigatoriosDevemEstarPreenchidos()
+            {
+                var pacienteService = new Mock<IPacienteService>();
+
+                var pacienteController = new PacienteController(pacienteService.Object);
+
+                var paciente = new Paciente();
+
+                var jsonResult = pacienteController.SalvaPaciente(paciente);
+
+                var jObject = JObject.FromObject(jsonResult.Data);
+                var mensagensErro = jObject["mensagensErro"].Value<JArray>();
+
+                Assert.IsTrue(mensagensErro.Count == 2);
+            }
+        }
+    }
+}
